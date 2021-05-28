c88bb4be0e4a4ab0257dae19ddb41f78ab41d7b8
unknownSheep <eric.haarlemmer@gmail.com>
2021-05-28 11:38:40 +0200

    test commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import game
 
 
 if __name__ == '__main__':
+    print("welcome")
     window = game.Window(1500, 1000, vsync=True, fullscreen=False)
     # window = game.Window(vsync=True, fullscreen=True)
     pyglet.clock.schedule_interval(window.update, 1 / 120)
