2285ba2b1ad97b24990f6894f3993ecf062d7ed1
unknownSheep <eric.haarlemmer@gmail.com>
2021-05-30 10:55:16 +0200

    hey

--- test.py
+++ test.py
@@ -1,16 +1,1 @@
-import pyglet
-import pygame
-import math
-import datetime
-import utils
-vec2 = pygame.math.Vector2
 
-t = utils.float_time()
-print(t)
-
-input(">>")
-
-y = utils.float_time()
-
-print(y)
-print(y-t)
